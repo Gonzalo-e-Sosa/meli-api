a6560e11465f63e4e83137afac7433140a5a1844
Gonza <gonzalososa342@gmail.com>
2024-04-01 23:58:10 -0300

    config project

---
+++ config.ts
@@ -0,0 +1,9 @@
+export const API_BASE_URL = import.meta.env.API_URL || null;
+export const SELLER_ID = {
+    name: "seller_id",
+    exampleValue: "179571326"
+}
+export const CATEGORY_ID = {
+    name: "category_id",
+    exampleValue: "0"
+}
